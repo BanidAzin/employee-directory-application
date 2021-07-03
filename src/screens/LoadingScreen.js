a7942ebc2b62811d855cc7bf719788f91dba6548
banidazin <banid@iocod.com>
2021-07-03 15:19:50 +0530

    complete loading screen

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const LoadingScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>LoadingScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Employee Directory</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30.0,
+    fontStyle: 'italic',
+  },
+});
